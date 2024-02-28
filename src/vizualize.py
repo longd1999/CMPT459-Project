bd0eb1b04e5e26217d94a90695ba2df431ab5286
Tristian <tristian.labanowich@gmail.com>
2024-02-28 15:39:18 -0800

    get basic stats for data

--- src/vizualize.py
+++ src/vizualize.py
@@ -8,7 +8,36 @@ testData = pd.read_csv('../dataset/cases_2021_test.csv')
 location = pd.read_csv('../dataset/location_2021.csv')
 fullSet = pd.concat([trainData,testData], ignore_index=True)
 fullSet = fullSet.groupby("country")
+# 11 cols
+
+def parsePercentages(group):
+    counts = {
+        "age" : 0,
+        "sex" :0,
+        "province":0,
+        "country":0,
+        "latitude":0,
+        "longitude":0,
+        "date_confirmation":0,
+        "additional_information":0,
+        "source":0,
+        "chronic_disease_binary":0,
+        "outcome_group":0
+    }
+    for column in group.columns:
+        counts[column] = group[column].count()
+    percentages = {field: (count /  len(group)) * 100 for field, count in counts.items()}
+    return counts, percentages
+
+results = {}
 
 for country, group in fullSet:
-    print("Country:", country)
-    print(group)
+    counts, percentages = parsePercentages(group)
+    results[country] = {
+        "DataSize": len(group),
+        "counts":counts,
+        "percentages":percentages
+    }
+
+print(results)
+
