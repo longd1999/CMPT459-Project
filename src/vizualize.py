b16271e224507c585d42cce4833b15b39520585a
Tristian <tristian.labanowich@gmail.com>
2024-02-28 16:26:06 -0800

    Add comments

--- src/vizualize.py
+++ src/vizualize.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sb
+import seaborn as sns
 
+# get and merge data
 trainData = pd.read_csv('../dataset/cases_2021_train.csv')
 testData = pd.read_csv('../dataset/cases_2021_test.csv')
 location = pd.read_csv('../dataset/location_2021.csv')
@@ -10,6 +11,7 @@ fullSet = pd.concat([trainData,testData], ignore_index=True)
 fullSet = fullSet.groupby("country")
 # 11 cols
 
+# helper function, this is what is counting and calculating data that is currently present
 def parsePercentages(group):
     counts = {}
     for column in group.columns:
@@ -23,16 +25,23 @@ results = {}
 for country, group in fullSet:
     counts, percentages, total_percent = parsePercentages(group)
     results[country] = {
-        "DataSize": len(group),
-        "counts":counts,
-        "percentages":percentages,
-        "overall data": total_percent
+        "DataSize": len(group), # total data size per country
+        "counts":counts, # counts data that exists vs what does not exist
+        "percentages":percentages, # calculates percentage of data that is avaialable for each category
+        "overall data": total_percent # calculates an overall "percent" of data that is present
     }
 
+# Convert to data frame to work with data properly, need to use Transpose to set country as col. 
 results = pd.DataFrame(results).T
 
+# Generic plotting
 plt.figure(figsize=(10, 8))
 plt.barh(results.index, results['overall data'])
 plt.xlabel('Overall Data (%)')
 plt.title('Overall Data Percentage by Country')
 plt.savefig('results.svg')
+
+
+# TODO: switch current bar chart to use seaborn https://seaborn.pydata.org/examples/part_whole_bars.html
+# TODO: Group countries into "continents" which is not a current param in data
+# TODO: Create heat map, the data that is currently in "results" should hopefully make that pretty easy
