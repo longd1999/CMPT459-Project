793e03c08f9eba4b99dc79cf07bd8964eb610be8
Tristian <tristian.labanowich@gmail.com>
2024-03-05 17:58:06 -0800

    push to pull

--- src/vizualize.py
+++ src/vizualize.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import geopandas as gpd
 
 # get and merge data
 trainData = pd.read_csv('../dataset/cases_2021_train.csv')
@@ -37,14 +38,48 @@ for country, group in fullSet:
 
 # Convert to data frame to work with data properly, need to use Transpose to set country as col. 
 results = pd.DataFrame(results).T
-print(results)
-#Generic plotting
+
+# For bar plot of continents
+
 plt.figure(figsize=(10, 8))
 sns.barplot(x=results['overall data'], y=results.index)
 plt.xlabel('Overall Data (%)')
 plt.ylabel('Continent')
 plt.title('Overall Data Percentage by Country')
 plt.savefig('results.svg')
+
+
+
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+
+# Generate random values for each country
+np.random.seed(0)  # Set seed for reproducibility
+world['random_value'] = np.random.rand(len(world))
+
+# Plotting
+fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+world.plot(column='random_value', cmap='YlOrRd', linewidth=0.8, ax=ax, edgecolor='0.8')
+ax.axis('off')
+
+fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+world.plot(column='random_value', cmap='YlOrRd', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+ax.axis('off')
+
+# Add a title
+ax.set_title('World Heat Map with Random Values for Each Country', fontdict={'fontsize': '18', 'fontweight' : '3'})
+
+
+# Show the plot
+plt.savefig('temp.svg')
+
+# Load population data (assuming it's in a DataFrame called population_data)
+# You might need to adjust the column names accordingly
+population_data = results['DataSize']
+
+# Merge population data with world map data
+world = world.merge(population_data, how='left', left_on='name', right_index=True)
+
 
 
 # TODO: switch current bar chart to use seaborn https://seaborn.pydata.org/examples/part_whole_bars.html
